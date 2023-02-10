214e561150991526672525d1634a4fbcfea32a9d
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-02-10 16:56:48 +0100

    simple api calls work

--- inlämning1/main.js
+++ inlämning1/main.js
@@ -1,27 +1,61 @@
-const api_url = "https://pixabay.com/api/33510753-f6643855c5ea09942a44e3e4b/pictures?result=10";
+const api_url = "https://pixabay.com/api?key=33510753-f6643855c5ea09942a44e3e4b";
 
-async function fetchData()
-{
-let response = await fetch (api_url)
-let data = await response.json();
-return data;
+async function fetchData() {
+    let response = await fetch(api_url)
+    let data = await response.json();
+    return data;
 }
-let form = document.querySelector("form");
+
+function CreatePicture(data) {
+    let pictureList = document.querySelector("#pictureList");
+    let newEntry = document.createElement("li");
+    let picture = document.createElement("img");
+    let tags = document.createElement("p");
+    let author = document.createElement("p");
+
+    picture.src = data.webformatURL;
+    tags.innerText = data.tags;
+    author.innerText = data.user;
+
+
+    newEntry.append(picture)
+    newEntry.append(tags)
+    newEntry.append(author)
+
+    pictureList.append(newEntry);
+}
+
+
+async function ShowNewData(data) {
+    for (let item of data.hits) {
+        CreatePicture(item)
+    }
+
+}
+
 
 
 let searchTerm = "";
 let selectedColor = "";
 
-form.onsubmit = event => {
+
+
+
+
+let form = document.querySelector("form");
+form.onsubmit = async event => {
     event.preventDefault();
 
     searchTerm = form.searchQuerry.value;
     selectedColor = form.colorSelect.value;
 
-let pictureList = document.querySelector("#pictureList")
+    let pictureList = document.querySelector("#pictureList");
 
-for(let item of pictureList.children){
-    item.remove();
-}
+    for (let item of pictureList.children) {
+        item.remove();
+    }
+    let x = fetchData();
+    ShowNewData(await x)
+
 };
 
