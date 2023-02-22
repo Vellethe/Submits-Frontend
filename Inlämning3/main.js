7900f1f08d6666d3969376e29468025456c0acdb
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-02-22 14:25:49 +0100

    empty textbox value

--- Inlämning3/main.js
+++ Inlämning3/main.js
@@ -2,13 +2,13 @@ let tasks = document.getElementById("tasks")
 let textbox = document.getElementById("textbox")
 let button = document.getElementById("button")
 
-button.addEventListener("click", function()
-{
-    let newTask = document.createElement("li")
-    newTask.innerHTML = textbox.value;
-    tasks.appendChild(newTask);
-    textbox.value = "";
-});
+// button.addEventListener("click", function()
+// {
+//     let newTask = document.createElement("li")
+//     newTask.innerHTML = textbox.value;
+//     tasks.appendChild(newTask);
+//     textbox.value = "";
+// });
 
 function addListItem() {
     var userInput = document.getElementById("userInput").value;
@@ -18,6 +18,7 @@ function addListItem() {
     checkbox.type = "checkbox";
     listItem.insertBefore(checkbox, listItem.firstChild);
     document.getElementById("tasks").appendChild(listItem);
+    document.getElementById("userInput").value = "";
 }
 
   
