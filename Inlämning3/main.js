04540d5dae9ddf0cf1639af5a35932c29ed0d3a9
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-02-22 14:29:30 +0100

    .

--- Inlämning3/main.js
+++ Inlämning3/main.js
@@ -2,14 +2,6 @@ let tasks = document.getElementById("tasks")
 let textbox = document.getElementById("textbox")
 let button = document.getElementById("button")
 
-// button.addEventListener("click", function()
-// {
-//     let newTask = document.createElement("li")
-//     newTask.innerHTML = textbox.value;
-//     tasks.appendChild(newTask);
-//     textbox.value = "";
-// });
-
 function addListItem() {
     var userInput = document.getElementById("userInput").value;
     var listItem = document.createElement("li");
