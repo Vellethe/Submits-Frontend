b525d1f9d135e8f9b5f61eda3839530306516075
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-02-22 13:01:10 +0100

    Checkbox fixing

--- Inlämning3/main.js
+++ Inlämning3/main.js
@@ -9,3 +9,15 @@ button.addEventListener("click", function()
     tasks.appendChild(newTask);
     textbox.value = "";
 });
+
+function addListItem() {
+    var userInput = document.getElementById("userInput").value;
+    var listItem = document.createElement("li");
+    var checkbox = document.createElement("input");
+    listItem.textContent = userInput;
+    checkbox.type = "checkbox";
+    listItem.insertBefore(checkbox, listItem.firstChild);
+    document.getElementById("tasks").appendChild(listItem);
+}
+
+  
