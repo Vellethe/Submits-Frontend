635259740ced24c479263a62c05e1c70d01faeea
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-02-24 17:16:02 +0100

    Trashcan removed

--- Inlämning3/main.js
+++ Inlämning3/main.js
@@ -3,26 +3,21 @@ let textbox = document.getElementById("textbox")
 let button = document.getElementById("button")
 
 function addListItem() {
-    let userInput = document.getElementById("userInput").value;
-    let listItem = document.createElement("li");
-    let checkbox = document.createElement("input");
-    let deleteButton = document.createElement("button");
-  
-    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-  
-    deleteButton.onclick = function() {
+  let userInput = document.getElementById("userInput").value;
+  let listItem = document.createElement("li");
+  let checkbox = document.createElement("input");
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "delete";
+  deleteButton.onclick = function()
+  {
       this.parentNode.remove();
-    };
-    
-    listItem.textContent = userInput;
-    checkbox.type = "checkbox";
-    listItem.insertBefore(checkbox, listItem.firstChild);
-    listItem.appendChild(deleteButton);
-    document.getElementById("tasks").appendChild(listItem);
-    document.getElementById("userInput").value = "";
-  
-    deleteButton.classList.add("delete");
-    checkbox.classList.add("checkbox");
-  }
+  };
+  listItem.textContent = userInput;
+  checkbox.type = "checkbox";
+  listItem.insertBefore(checkbox, listItem.firstChild);
+  listItem.appendChild(deleteButton);
+  document.getElementById("tasks").appendChild(listItem);
+  document.getElementById("userInput").value = "";
+}
 
   
