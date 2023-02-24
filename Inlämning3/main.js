09f3f1786bcb8dafbb9f8db31bdc56b35b53c01b
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-02-24 13:29:13 +0100

    Failed trashcanIcon

--- Inlämning3/main.js
+++ Inlämning3/main.js
@@ -7,17 +7,22 @@ function addListItem() {
     let listItem = document.createElement("li");
     let checkbox = document.createElement("input");
     let deleteButton = document.createElement("button");
-    deleteButton.textContent = "delete";
-    deleteButton.onclick = function()
-    {
-        this.parentNode.remove();
+  
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+  
+    deleteButton.onclick = function() {
+      this.parentNode.remove();
     };
+    
     listItem.textContent = userInput;
     checkbox.type = "checkbox";
     listItem.insertBefore(checkbox, listItem.firstChild);
     listItem.appendChild(deleteButton);
     document.getElementById("tasks").appendChild(listItem);
     document.getElementById("userInput").value = "";
-}
+  
+    deleteButton.classList.add("delete");
+    checkbox.classList.add("checkbox");
+  }
 
   
