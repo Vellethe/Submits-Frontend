928e9c622898901cba08a7bd1a0d003e8c3cd0d1
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-03-13 13:52:14 +0100

    added some seed data and some things for local storage

--- inlämning4/main.js
+++ inlämning4/main.js
@@ -1,8 +1,29 @@
+function getSeedData() {
+    let seedData = {
+        "data": [
+            {
+                "transactionAmount": 20,
+                "transactionDate": "2023-03-13",
+                "transactionName": "apple"
+            },
+            {
+                "transactionAmount": 20,
+                "transactionDate": "2023-03-14",
+                "transactionName": "book"
+            }
+        ]
+    }
+    return seedData.data;
+}
+
+
+
+
 const app = Vue.createApp({
     data() {
         return {
-            testNum:0
-            
+            testNum: 0,
+            transactionList: this.readFromLocalStorage() ?? getSeedData()
 
 
 
@@ -12,10 +33,19 @@ const app = Vue.createApp({
 
         }
     },
-    methods:{
-        test(){
+    methods: {
+        writeToLocalStorage(data) {
+            let stringJson = JSON.stringify(data);
+            window.localStorage.setItem("data", stringJson);
+        },
+        readFromLocalStorage() {
+            JSON.parse(window.localStorage.getItem("data"));
+        },
+
+        test() {
             this.testNum++;
-            console.log("this is test"+ this.testNum);
+            console.log("this is test" + this.testNum);
+            console.log(this.transactionList)
         }
     }
 }).mount('#app');
