83c93247c26cd04216929dcb712dad4f078b1da8
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-03-15 03:00:01 +0100

    Work with a pie diagram

--- inlämning4/main.js
+++ inlämning4/main.js
@@ -4,17 +4,38 @@ function getSeedData() {
             {
                 "transactionAmount": 20,
                 "transactionDate": "2023-03-13",
-                "transactionName": "apple"
+                "transactionName": "apple",
+                "transactionCategory": "food"
             },
             {
                 "transactionAmount": 20,
                 "transactionDate": "2023-03-14",
-                "transactionName": "book"
+                "transactionName": "book",
+                "transactionCategory": "fun"
             }
         ]
     }
     return seedData.data;
 }
+
+function degreeToRadian(degree) {
+    return (Math.PI / 180) * degree
+}
+
+function makePieSlice(c,startAngle, endAngle,centerX,centerY,radius, color) {
+    c.beginPath();
+    c.moveTo(centerX, centerY);
+
+
+    c.arc(centerX, centerY, radius, degreeToRadian(startAngle-90), degreeToRadian(endAngle-90));
+    c.closePath();
+    c.fillStyle = "green";
+    c.fill();
+    c.stroke();
+
+
+}
+
 
 const app = Vue.createApp({
     data() {
@@ -25,7 +46,7 @@ const app = Vue.createApp({
             name: "",
             amount: 0,
             date: "",
-            totalSpent:
+            totalSpent: 0
 
         }
     },
@@ -33,26 +54,39 @@ const app = Vue.createApp({
         addExpense() {
             let newExpense = {
                 name: this.name,
-                amount: this.amount, 
+                amount: this.amount,
                 date: this.date
             }
             this.transactionList.push(newExpense),
-            this.writeToLocalStorage();
+                this.writeToLocalStorage();
             this.name = '',
-            this.amount = 0,
-            this.date = ''
+                this.amount = 0,
+                this.date = ''
         },
 
 
 
 
+        updatePie() {
+            let canvas = document.querySelector("#pieCanvas");
+            let c = canvas.getContext("2d");
+            let radius = 50;
+            let centerOfPie = 100;
+            let rotation = 0;
 
+            makePieSlice(c,0,90,centerOfPie,centerOfPie,radius,"");
+
+        },
         writeToLocalStorage(data) {
             let stringJson = JSON.stringify(data);
             window.localStorage.setItem("data", stringJson);
         },
         readFromLocalStorage() {
-            JSON.parse(window.localStorage.getItem("data"));
+            if (window.localStorage.getItem("data") !== "undefined") {
+
+                return JSON.parse(window.localStorage.getItem("data"));
+            }
+            return undefined;
         },
     }
 }).mount('#app');
