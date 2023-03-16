3c3fc6e8abc64c6b736bbbe416af819b7bc6bada
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-03-16 15:10:56 +0100

    local storage works

--- inlämning4/main.js
+++ inlämning4/main.js
@@ -22,12 +22,12 @@ function degreeToRadian(degree) {
     return (Math.PI / 180) * degree
 }
 
-function makePieSlice(c,startAngle, endAngle,centerX,centerY,radius, color) {
+function makePieSlice(c, startAngle, endAngle, centerX, centerY, radius, color) {
     c.beginPath();
     c.moveTo(centerX, centerY);
 
 
-    c.arc(centerX, centerY, radius, degreeToRadian(startAngle-90), degreeToRadian(endAngle-90));
+    c.arc(centerX, centerY, radius, degreeToRadian(startAngle - 90), degreeToRadian(endAngle - 90));
     c.closePath();
     c.fillStyle = "green";
     c.fill();
@@ -51,7 +51,7 @@ const app = Vue.createApp({
     },
 
     computed: {
-        totalSpentFormatted(){
+        totalSpentFormatted() {
             return this.totalSpent.toFixed(0) + "kr";
         }
     },
@@ -64,27 +64,26 @@ const app = Vue.createApp({
                 amount: parseFloat(this.amount),
                 date: this.date
             }
-            this.transactionList.push(newExpense),
-                this.writeToLocalStorage();
-                this.name = '',
-                this.amount = 0,
-                this.date = ''
-                // reduce method sums up the amount of each expense in transactionList and then adds it to totalSpent 
-                this.totalSpent = this.transactionList.reduce((total, expense) => total + expense.amount, 0)
-        },
-
-        deleteExpense(index) 
-        {
-            this.transactionList.splice(index, 1);
-            this.writeToLocalStorage(); 
+            this.transactionList.push(newExpense);
+            this.writeToLocalStorage(this.transactionList);
+            this.name = '';
+            this.amount = 0;
+            this.date = '';
+            // reduce method sums up the amount of each expense in transactionList and then adds it to totalSpent 
             this.totalSpent = this.transactionList.reduce((total, expense) => total + expense.amount, 0)
         },
-        
+
+        deleteExpense(index) {
+            this.transactionList.splice(index, 1);
+            this.writeToLocalStorage();
+            this.totalSpent = this.transactionList.reduce((total, expense) => total + expense.amount, 0)
+        },
+
         computed: {
-            sortedItems: function() {
-              return this.items.sort((a, b) => new Date(a.date) - new Date(b.date))
+            sortedItems: function () {
+                return this.items.sort((a, b) => new Date(a.date) - new Date(b.date))
             }
-          },
+        },
 
 
 
@@ -96,7 +95,7 @@ const app = Vue.createApp({
             let centerOfPie = 100;
             let rotation = 0;
 
-            makePieSlice(c,0,90,centerOfPie,centerOfPie,radius,"");
+            makePieSlice(c, 0, 90, centerOfPie, centerOfPie, radius, "");
 
         },
         writeToLocalStorage(data) {
